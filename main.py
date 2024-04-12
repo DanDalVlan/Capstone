4f91e1584b02ba874027b5f40209a70e6835aeb8
Dan Monahan <dandalvlan@hotmail.com>
2024-04-12 13:40:18 -0400

    Testing still

--- main.py
+++ main.py
@@ -4,21 +4,26 @@ from pandas.plotting import scatter_matrix
 from sklearn import linear_model
 
 # importing raw data
-url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_final.csv"
-names = ['Track', 'Artist', 'Genre', 'Valence', 'Arousal', 'Dominance']
+# url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_final.csv"
+# names = ['Track', 'Artist', 'Genre', 'Valence', 'Arousal', 'Dominance']
+# df = pd.read_csv(url, names=names)
+
+url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_test.csv"
+names = ['Valence', 'Arousal', 'Dominance', 'Track']
 df = pd.read_csv(url, names=names)
 
 # creating a pyplot
-#df.hist()
-#pyplot.show()
+# df.hist()
+# pyplot.show()
 
 # creating a scatter matrix
 # scatter_matrix(df)
 
 # creating a model
 mylog_model = linear_model.LogisticRegression()
-x = df[['Valence', 'Arousal', 'Dominance']]
-y = df[:0]
+y = df.values[:, 3]
+x = df.values[:, 0:3]
+
 
 # training the model
 mylog_model.fit(x, y)
