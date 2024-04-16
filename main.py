3e52a589a0b793151063f2ab66cdc049b41d765b
Dan Monahan <dandalvlan@hotmail.com>
2024-04-16 12:49:57 -0400

    First official commit - Data works, graphs work, program runs with no errors.

--- main.py
+++ main.py
@@ -1,33 +1,151 @@
 import pandas as pd
-from matplotlib import pyplot
-from pandas.plotting import scatter_matrix
-from sklearn import linear_model
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn import svm, model_selection, metrics
+from sklearn.metrics import RocCurveDisplay
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
 # importing raw data
-url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_final.csv"
+url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/data_final_no_headers.csv"
 names = ['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave Points',
          'Symmetry', 'Fractal Dimension', 'Diagnosis']
 df = pd.read_csv(url, names=names)
 
-url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/capstone_data_short.csv"
-names = ['Valence', 'Arousal', 'Dominance', 'Track']
-df = pd.read_csv(url, names=names)
+# Define the features and target
+X = df[['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave Points',
+        'Symmetry', 'Fractal Dimension']].values
+y = df['Diagnosis'].values
+X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.3)
 
-# creating a pyplot
-# df.hist()
-# pyplot.show()
+# Create a pipeline that scales the data and then fits the SVM model
+mysvm_model = make_pipeline(StandardScaler(), svm.SVC(max_iter=1000))
 
-# creating a scatter matrix
-# scatter_matrix(df)
+# Train the model
+mysvm_model.fit(X_train, y_train)
 
-# creating a model
-mylog_model = linear_model.LogisticRegression()
-y = df.values[:, 3]
-x = df.values[:, 0:3]
+# Predictions
+y_pred_svm = mysvm_model.predict(X_test)
 
 
-# training the model
-mylog_model.fit(x, y)
+# Function to ensure valid user input
+def get_valid_input(prompt, min_value, max_value):
+    while True:
+        user_input = input(prompt)
+        try:
+            user_input = float(user_input)
+            if min_value <= user_input <= max_value:
+                return user_input
+            else:
+                print(f"Please enter a number between {min_value} and {max_value}")
+        except ValueError:
+            print("Please enter an integer")
 
-# making predictions
-print(mylog_model.predict([[3, 5, 5]]))
+
+# Function that creates and shows the graph of characteristics and their importance to the model
+def importance_graph():
+    svm_graph_model = SVC(kernel='linear', random_state=0)
+    svm_graph_model.fit(X_train, y_train)
+
+    tumor_list = df.columns[:-1]
+    list_importance = svm_graph_model.coef_[0]
+    sorted_list = np.argsort(np.abs(list_importance))
+
+    plt.figure(figsize=(10, 6))
+    plt.barh(range(len(sorted_list)), np.abs(list_importance[sorted_list]), color="blue")
+    plt.yticks(range(len(sorted_list)), tumor_list[sorted_list])
+    plt.title('The Importance of Each Tumor Characteristic to the Model')
+    plt.xlabel('Importance')
+    plt.ylabel('Characteristics')
+    plt.show()
+
+
+# Function that creates and shows a pair plot
+def pairplot_graph():
+    sns.pairplot(df, hue='Diagnosis', markers=["o", "s"], palette="Set1")
+    plt.suptitle('Pair Plot of Tumor Characteristics by Diagnosis', size=16)
+    plt.subplots_adjust(top=0.95)
+    plt.show()
+
+
+# Function that creates and shows a ROC Curve
+def roc_graph():
+    RocCurveDisplay.from_estimator(mysvm_model, X_test, y_test)
+    plt.title('ROC Curve')
+    plt.show()
+
+
+# Main Function that contains all user interactions
+class Main:
+    print("\nHello and welcome to the McGuffin Institute's Tumor Diagnostic Project")
+    print("You will be prompted for the following 10 tumor characteristic values, please ensure you have them on hand:")
+    print("1. Radius\n2. Texture\n3. Perimeter\n4. Area\n5. Smoothness\n6. Compactness\n7. Concavity\n8. Concave points"
+          "\n9. Symmetry\n10. Fractal Dimension\n")
+    input_selection = 0
+    while True:
+        input_selection = get_valid_input("\nSelect an option:\n1. Begin Diagnostics\n2. Verify Methodology\n"
+                                          "3. End Diagnostics\n", 1, 3)
+
+        # If user selects 'Begin Diagnostic', prompt them for all the inputs and show prediction
+        if input_selection == 1:
+            # Receive inputs from the users
+            radius = get_valid_input("1. Radius in mm (mean of distances from center to points on the "
+                                     "perimeter): ", 0, 50)
+            texture = get_valid_input("2. Texture (standard deviation of gray-scale values): ",
+                                      0, 50)
+            perimeter = get_valid_input("3. Perimeter in mm: ", 0, 300)
+            area = get_valid_input("4. Area in mm: ", 0, 5000)
+            smoothness = get_valid_input("5. Smoothness (local variation in radius lengths): ",
+                                         0, 1)
+            compactness = get_valid_input("6. Compactness (perimeter^2 / area - 1.0): ", 0, 1)
+            concavity = get_valid_input("7. Concavity (severity of concave portions of the contour): ",
+                                        0, 1)
+            concave_points = get_valid_input("8. Concave Points (number of concave portions of the contour): ",
+                                             0, 1)
+            symmetry = get_valid_input("9. Symmetry: ", 0, 1)
+            fractal_dimension = get_valid_input("10. Fractal Dimensions ('coastline approximation' - 1): ",
+                                                0, 1)
+
+            # Creating and formatting the prediction
+            prediction_base = mysvm_model.predict([[radius, texture, perimeter, area, smoothness, compactness,
+                                                    concavity, concave_points, symmetry, fractal_dimension]])
+            if prediction_base == 'M':
+                prediction = 'Malignant'
+            else:
+                prediction = 'Benign'
+
+            # Formatting and printing the prediction/accuracy
+            accuracy = metrics.accuracy_score(y_test, y_pred_svm) * 100
+            print("\nOur diagnostic prediction for the tumor whose characteristics were provided is: ", prediction)
+            print("This model has an accuracy of: {:.2f}%".format(accuracy))
+
+        # Else if user selects 'Verify Methodology', show next menu for visualizations
+        elif input_selection == 2:
+            while True:
+                input_visuals = get_valid_input("Which visualization would you like to look at?\n"
+                                                "1. Importance Graph\n2. Pair Plot\n3. ROC Curve\n"
+                                                "4. Return to previous menu\n", 1, 4)
+                if input_visuals == 1:
+                    importance_graph()
+
+                elif input_visuals == 2:
+                    pairplot_graph()
+
+                elif input_visuals == 3:
+                    roc_graph()
+
+                elif input_visuals == 4:
+                    break
+
+                else:
+                    print("Invalid selection")
+
+        # Else if user selects 'End Diagnostics', break loop
+        elif input_selection == 3:
+            break
+
+        # Else print 'Invalid selection'
+        else:
+            print("Invalid selection")
