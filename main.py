8dc391c52da79178335aacc929bf51ba7502bd35
Dan Monahan <dandalvlan@hotmail.com>
2024-04-16 09:51:33 -0400

    CSV

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from pandas.plotting import scatter_matrix
 from sklearn import linear_model
 
 # importing raw data
-# url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_final.csv"
-# names = ['Track', 'Artist', 'Genre', 'Valence', 'Arousal', 'Dominance']
-# df = pd.read_csv(url, names=names)
+url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/muse_v3_final.csv"
+names = ['Radius', 'Texture', 'Perimeter', 'Area', 'Smoothness', 'Compactness', 'Concavity', 'Concave Points',
+         'Symmetry', 'Fractal Dimension', 'Diagnosis']
+df = pd.read_csv(url, names=names)
 
 url = "https://raw.githubusercontent.com/DanDalVlan/Capstone/working/capstone_data_short.csv"
 names = ['Valence', 'Arousal', 'Dominance', 'Track']
